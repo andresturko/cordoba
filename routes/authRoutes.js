122ad400819ced16e0b04efc81cf3823bc6a46ce
Andres Turko <andresnicolasturko@gmail.com>
2024-09-25 18:02:14 -0300

    1er entregable con register/login y CRUD completo protegido con JWT

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { register, login } from '../controllers/authController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js'; '../middlewares/authMiddleware.js';
 import client from '../config/mongodb.js';
+import bcrypt from 'bcryptjs';
 //const express = require('express');
 //const authController = require('../controllers/authController');
 //const authMiddleware = require('../middlewares/authMiddleware');
@@ -33,7 +34,7 @@ router.get('/protected', authMiddleware, async (req, res) => {
 
       } catch (error) {
             console.log("todo mal loco", error);
-      } 
+      }
       //finally {
       //      await client.close();
       //}
@@ -48,7 +49,7 @@ router.get('/protected/:username', authMiddleware, async (req, res) => {
             const database = client.db("cordoba");
             const nombres = database.collection("users");
 
-            // Query for a movie that has the title 'The Room'
+            // Query
             const query = { "username": req.params.username };
 
             const options = {
@@ -76,6 +77,60 @@ router.get('/protected/:username', authMiddleware, async (req, res) => {
       // finally {
       //      await client.close();
       //}
+}
+);
+
+router.delete('/protected/:username', authMiddleware, async (req, res) => {
+
+      try {
+            // Get the database and collection on which to run the operation
+            const database = client.db("cordoba");
+            const nombres = database.collection("users");
+
+            // Query
+            const query = { "username": req.params.username };
+            const borrar = await nombres.deleteOne(query);
+
+            //response and details
+            if (borrar.deletedCount == 0) {
+                  console.log("no se encontro el usuario", query.username);
+                  res.status(400).send(`no se encontro el usuario: ${query.username}`)
+            } else {
+
+                  console.log(borrar);
+                  res.status(201).json({ "se ha borrado el usuario:": query.username });
+            }
+
+      } catch (error) {
+
+            console.log("no se borro ningun usuario", error);
+            res.status(400).send("no se borro ningun usuario");
+
+      }
+}
+);
+
+router.put('/protected/:username', authMiddleware, async (req, res) => {
+
+      const database = client.db("cordoba");
+      const recurso = database.collection("users");
+
+      const documento = { "username": req.params.username };
+      const nuevaPassword = req.body.password;
+      const encriptacion = bcrypt.hashSync(nuevaPassword, 8);
+      const actualizacion = { $set: {"password": encriptacion} };
+
+      console.log(documento, actualizacion)
+
+
+      try {
+            const actualizar = await recurso.updateOne(documento, actualizacion);
+            res.status(200).send("hecho");
+
+      } catch (error) {
+            console.log("no se actualizo ningun usuario", error);
+            res.status(400).send("no se actualizo ningun usuario");
+      }
 }
 );
 
